05c7a4b7ab72ad3b49099426dc42806a64b834da
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-10-21 17:07:12 +0400

    Updated entity classes

--- ms-account/src/main/java/az/edu/turing/msaccount/domain/entity/UserEntity.java
+++ ms-account/src/main/java/az/edu/turing/msaccount/domain/entity/UserEntity.java
@@ -9,9 +9,12 @@ import lombok.Data;
 @Data
 @Entity
 public class UserEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String username;
+
     private String password;
 }
